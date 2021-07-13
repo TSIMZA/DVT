b1aa1bef720cbacaa3de57ac95977fb28503add3
TSIMZA <41180435+TSIMZA@users.noreply.github.com>
2021-07-13 20:31:09 +0200

    Initial
    Initial

---
+++ DVTAssessment/Program.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVTAssessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<string> a = new List<string>();
+
+            for (int i = 0; i < aCount; i++)
+            {
+                string aItem = Console.ReadLine();
+                a.Add(aItem);
+            }
+
+            int bCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<string> b = new List<string>();
+
+            for (int i = 0; i < bCount; i++)
+            {
+                string bItem = Console.ReadLine();
+                b.Add(bItem);
+            }
+
+            commonSubstring(a, b);
+        }
+
+        private static readonly object _Lock = new object();
+        public static void commonSubstring(List<string> a, List<string> b)
+        {
+            //aCount and bCount are said to be both "n" from the instructions byt inputs allow them to be different from main
+            //Thus I'll be finding the highest common index from both list to avoid and IndexOutOfBounds exception
+            int maxCommonItemsCount = a.Count > b.Count ? b.Count : a.Count;
+            Dictionary<int, string> result = new Dictionary<int, string>();
+
+            Parallel.For(0, maxCommonItemsCount, i =>
+            {
+                //for (int i = 0; i < maxCommonItemsCount; i++)
+                //{
+                IEnumerable<string> aCombinations = from x in Enumerable.Range(0, a[i].Length)
+                                                    from y in Enumerable.Range(0, a[i].Length - x + 1)
+                                                    where y > 0
+                                                    select a[i].Substring(x, y);
+
+                IEnumerable<string> bCombinations = from x in Enumerable.Range(0, b[i].Length)
+                                                    from y in Enumerable.Range(0, b[i].Length - x + 1)
+                                                    where y > 0
+                                                    select b[i].Substring(x, y);
+
+                //bool containsValue = false;
+                //foreach(string value in aCombinations)
+                //{
+                //    b[i].Contains(value);
+                //    if (b[i].Contains(value))
+                //    {
+                //        containsValue = true;
+                //        break;
+                //    }
+                //}
+                IEnumerable<string> afd = aCombinations.Intersect(bCombinations);
+
+                lock (_Lock)
+                {
+                    result.Add(i, afd.Any() ? "YES" : "NO");
+                }
+
+                //}
+            });
+
+            Console.WriteLine(string.Join("\n", result.OrderBy(k => k.Key).Select(v => v.Value)));
+
+            //Console.WriteLine(afd.Any() ? "YES" : "NO");
+        }
+    }
+}
---
+++ Threshold/Program.cs
@@ -0,0 +1,116 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Threshold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int threshold = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<string> result = getUsernames(threshold);
+
+            Console.WriteLine($"{string.Join("\n", result)}");
+
+            //textWriter.WriteLine(String.Join("\n", result));
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+
+        public static List<string> getUsernames(int threshold)
+        {
+            HttpClient client = new HttpClient()
+            {
+                BaseAddress = new Uri("https://jsonmock.hackerrank.com/api/"),
+            };
+
+            List<string> userNames = new List<string>();
+            int pageNumber = 1;
+            bool endLoop = false;
+            while(!endLoop)
+            {
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"article_users?page={pageNumber}");
+
+                HttpResponseMessage response = client.SendAsync(request).GetAwaiter().GetResult();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    UserSubmissionResponse responseData = response.Content.ReadAsAsync<UserSubmissionResponse>().GetAwaiter().GetResult();
+
+                    foreach (UserSubmission submission in responseData.data)
+                    {
+                        if (submission.submitted > threshold)
+                        {
+
+                            if (userNames.Count == threshold)
+                                break;
+
+                            userNames.Add(submission.username);
+                        }
+                    }
+
+                    endLoop = (pageNumber == responseData.total_pages) || (userNames.Count == threshold);
+                }
+                else
+                {
+                    Console.WriteLine("Failed to get results");
+                }
+
+                pageNumber++;
+            }
+            
+
+            return userNames;
+        }
+    }
+
+
+    public class UserSubmissionResponse
+    {
+
+        public int per_page { get; set; }
+
+        public int total { get; set; }
+
+        public int total_pages { get; set; }
+
+        public IEnumerable<UserSubmission> data { get; set; }
+    }
+
+    public class UserSubmission
+    {
+        public int id { get; set; }
+
+        public string username { get; set; }
+
+        public int submitted { get; set; }
+
+        public string about { get; set; }
+
+       public DateTimeOffset updated_at { get; set; }
+
+        public int submission_count { get; set; }
+
+        public int comment_count { get; set; }
+
+        //public DateTimeOffset created_at { get; set; }
+    }
+}
